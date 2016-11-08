18fe3859a5f928bcb926aacb8eba17f8725d1c8e
Howard19960920 <hower.song@gmail.com>
2016-11-08 23:33:17 +0800

    Insert data into RETHINK while user inputing in the front.

--- server/ws-router.js
+++ server/ws-router.js
@@ -1,42 +1,61 @@
 import Router from 'koa-router'
 import Pool from './websocket/Pool'
-
+import r from 'rethinkdb'
 const router = new Router()
 const pool = new Pool()
-let id = 0
+//let id = 0
 
 const actions = {
-  register (socket, payload) {
-    socket.send(JSON.stringify({
-      event: 'register',
-      id
-    }))
-    pool.boardcast(JSON.stringify({
-      event: 'join',
-      payload: {
-        id,
-        name: payload
-      }
-    }))
-    socket.id = id
-    id += 1
+  register (socket, payload ) {
+    socket.id = payload
   },
-  message (socket, payload) {
+
+
+  async message (socket, payload) {
     console.log(`Socket ${socket.id} emit message: ${payload}`)
     pool.boardcast(JSON.stringify({
       event: 'message',
       id: socket.id,
       message: payload
     }))
-  }
+
+	const conn = await r.connect({db:'chatroom'})
+	const result = await r.table('message').insert({
+
+		"uid": socket.id,
+		"content": payload,
+	}).run(conn)
+	
+	if(result)	{
+		console.log("Insert successfully!")
+	
+	}		
+	conn.close()
+  },
+
+
+ 	async dump(socket, payload) {
+	
+	const conn = await r.connect( {db:'chatroom'}) 	
+	const all = await r.table('message').run(conn)
+
+	all.each((err ,val) =>{
+
+		if(err)	{
+			console.log(err)
+		}
+		console.log(val)
+
+	})
+
+
+	}	
 }
 
 router.get('/socket', (ctx) => {
   const remove = pool.register(ctx.websocket)
-  console.log('Socket connect')
   ctx.websocket.on('message', (content) => {
     const data = JSON.parse(content)
-    console.log(`Receive ${content}`)
     if (actions[data.event]) {
       actions[data.event](ctx.websocket, data.payload)
     }
